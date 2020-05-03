2afb39af88bc67aa21110ab44f1515eb042ea2a8
jaisondza <jaisonj1010@gmail.com>
2020-05-03 18:09:28 +0530

    add high and low temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,11 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, 
+                    body.daily.data[0].summary + ' It is currently ' + body.currently.temperature  
+                    + ' degress out. Today\'s highest temperature is ' + body.daily.data[0].temperatureHigh 
+                    + ' degrees and the Lowest temperature is '+ body.daily.data[0].temperatureLow 
+                    +' degrees. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }

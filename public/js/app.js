fce4fff96c0413518ffbb5bd6f63f59b3daf52a2
jaisondza <jaisonj1010@gmail.com>
2020-05-03 16:53:40 +0530

    App for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,7 @@ weatherForm.addEventListener('submit', ( e )=>{
     message.textContent = 'Loading...'
     message2.textContent = ''
 
-        fetch('http://localhost:3000/weather?address='+location).then((res)=>{
+        fetch('/weather?address='+location).then((res)=>{
         res.json().then((data)=>{
             if(data.error){
                 console.log(data.error)
